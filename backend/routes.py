7e7726982d3f60dda3a565bd2f193ef7491a948a
Ana Makarevich <electric.blake@gmail.com>
2021-11-20 12:41:22 +0200

    Add owned items route

--- backend/routes.py
+++ backend/routes.py
@@ -8,6 +8,52 @@ from models import Hackathon, hackathons_schema, hackathon_schema
 app = create_app()
 
 
+@app.route('/owned_items/<uid>')
+def owned_items(uid):
+    dummy_owned_items = [
+        {
+            "id": 1,
+            "date_added": "2021-10-10T00:00:00",
+            "name": "Skateboard",
+            "description": "A skateboard that I'm not using since I started to work at a big corporation.",
+            "owner": uid,
+            "state": "Heavily abused",
+            "available_since": "2021-12-12",
+            "max_rent_length": 90,
+            "kaution": 200,
+            "coins": 30,
+            "location": {
+                "lat": 48.1351253,
+                "lon": 11.5819806
+            }
+        },
+        {
+            "id": 2,
+            "date_added": "2021-10-10T00:00:00",
+            "name": "Travel suitcase",
+            "description": "A huge 100L travel suitcase that I only use once per  year",
+            "owner": uid,
+            "state": "Almost new",
+            "available_since": "2021-12-12",
+            "max_rent_length": 60,
+            "kaution": 200,
+            "coins": 60,
+            "location": {
+                "lat": 48.1351253,
+                "lon": 11.5819806
+            }
+        }
+    ]
+    return jsonify(dummy_owned_items)
+
+@app.route("/rented_items")
+def borrowed_items():
+    dummy_borrowed_items = {
+
+    }
+    return None
+
+
 @app.route('/', methods=["GET"], strict_slashes=False)
 def index():
     return "<h1>Hello</h1>"
