4f7a9805002415fd652573c1d1da92495df147f8
Aleksandr Zuev <zuev08@gmail.com>
2021-11-20 16:20:20 +0100

    add User, Item, Contract models

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,54 @@
 from app import db, ma
 
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), index=True, unique=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    coins = db.Column(db.Integer, default=100, nullable=False)
+    level = db.Column(db.String(100), default='padavan', nullable=False)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Item(db.Model):
+    __tablename__ = 'items'
+    id = db.Column(db.Integer, primary_key=True)
+    date_added = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text(), default='', nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    state = db.Column(db.String(100), default='', nullable=False)
+    availible_since = db.Column(db.DateTime, default=db.func.now())
+    max_rent_length = db.Column(db.Integer, default=90, nullable=False)
+    kaution = db.Column(db.Integer, default=0, nullable=False)
+    coins = db.Column(db.Integer, default=0, nullable=False)
+    lat = db.Column(db.Float, nullable=True)
+    lon = db.Column(db.Float, nullable=True)
+    required_post_actions = db.Column(db.Text, default='', nullable=False)
+    checked_at_return = db.Column(db.Text, default='', nullable=False)
+    image_url = db.Column(db.String(200), default='', nullable=False)
+
+    def __repr__(self):
+        return '<Item {}>'.format(self.name)
+
+
+class Contract(db.Model):
+    __tablename__ = 'contracts'
+    id = db.Column(db.Integer, primary_key=True)
+    provider_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    consumer_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), nullable=False)
+    start_date = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    end_date = db.Column(db.DateTime, default=db.func.now(), nullable=False)
+    status = db.Column(db.String(100), default='initial', nullable=False)
+    image_after_url = db.Column(db.String(200), nullable=True)
+    closed_on = db.Column(db.DateTime, nullable=True)
+
+
 class Hackathon(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
